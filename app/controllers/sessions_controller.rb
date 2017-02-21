a48d4cb4d757237c2c31891ed782bb827007afbf
supernovamaniac <alterprist@gmail.com>
2017-02-20 22:43:27 -0500

    flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,10 @@ class SessionsController < ApplicationController
   	user = User.find_by(username: params[:session][:username].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in(user)
+      flash[:success] = "Logged in!"
   		redirect_to user
   	else
+      flash[:warning] = "Invalid username/password combination"
   		render 'new'
   	end
   end
