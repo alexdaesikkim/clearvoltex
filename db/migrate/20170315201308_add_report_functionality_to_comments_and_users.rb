4ad581f0427d234f3cfb413c771f0d60167c2d0e
supernovamaniac <alterprist@gmail.com>
2017-04-03 19:56:58 -0400

    true 0.4 release (seeds fixed, and front page fixed a little)

--- db/migrate/20170315201308_add_report_functionality_to_comments_and_users.rb
+++ db/migrate/20170315201308_add_report_functionality_to_comments_and_users.rb
@@ -1,6 +1,10 @@
 class AddReportFunctionalityToCommentsAndUsers < ActiveRecord::Migration[5.0]
   def change
-  	add_column :comments, :reports, :integer, default: 0
-  	add_column :votes, :report, :boolean, default: false
+  	unless column_exists? :comments, :reports
+  		add_column :comments, :reports, :integer, default: 0
+  	end
+  	unless column_exists? :votes, :report
+  		add_column :votes, :report, :boolean, default: false
+  	end
   end
 end
