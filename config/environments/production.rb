9d02e104f5165a176a2ba7c09208415e60c09cb4
Alex Kim <alterprist@gmail.com>
2017-04-12 17:40:18 -0400

    mailer host changed, started working on revamping show page for users -still need pagination on users -figure out what to do with toggle button -user angular to update stats dynamically? look into angularjs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
+  # Setting host link for email
+  config.action_mailer.default_url_options = { host: 'clearvoltex.net'} 
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
